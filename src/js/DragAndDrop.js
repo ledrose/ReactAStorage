831edffb5c136b1280c2002d8289202c8bb4ddbc
ledrose <embre@bk.ru>
2021-12-13 19:58:08 +0700

    Добавлен альтернативный способ загрузки картинок

--- src/js/DragAndDrop.js
+++ src/js/DragAndDrop.js
@@ -1,57 +1,84 @@
 import React, { useState } from "react";
 import '../css/dragAndDrop.css';
-import { mainLink } from "./App";
+import { curToken, mainLink } from "./App";
 
+export default class DragAndDrop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      drag: false,
+      errorMessages:null,
+    }
+  }
+  sendImage = async (data) => {
+    var response = await fetch(mainLink+'/Files',{
+      method: "POST",
+      body: data,
+      headers: {
+        "Authorization":curToken.token
+      }
+    })
+    const responseBody= await response.json()
+    if (response.status==200) {
+      console.log(responseBody);
+    }
+    else {
+      console.log(responseBody);
+    }
+  }
 
-const DragAndDrop =() =>{
-  const [drag,setDrag] = useState(false)
-  function dragStartHandler(e) {
+  dragStartHandler = (e) => {
     e.preventDefault()
-    setDrag(true)
+    this.setState({drag:true})
   }
-  function dragLeaveHandler(e) {
+  dragLeaveHandler = (e) => {
     e.preventDefault()
-    setDrag(false)
+    this.setState({drag:false})
   }
-  function onDropHandler(e) {
-    e.preventDefault();
+  onDropHandler = (e) => {
+    e.preventDefault()
     let files = [...e.dataTransfer.files]
     files.forEach(el => {
       console.log(el)
       const formData = new FormData()
       formData.append('File',el,el.name);
-      fetch(mainLink+'/Files',{
-        method: "POST",
-        body: formData,
-      }).then(
-        async (response)=>{
-          console.log(await response.json())
-        },
-        async (response)=>{
-          console.log(await response.text())
-        }
-      )
+      this.sendImage(formData)
     });
-    setDrag(false)
+    this.setState({drag:false})
   }
-  return (
-    <div className="container drop_area_parent">
-      {drag
-      ?<div 
-        onDragStart={e=>dragStartHandler(e)}
-        onDragLeave={e=>dragLeaveHandler(e)}
-        onDragOver={e=>dragStartHandler(e)}
-        onDrop={e=>onDropHandler(e)}
-        className="drop_area_drag"
-      >Отпустите файлы, чтобы загрузить их</div>
-      :<div
-        onDragStart={e=>dragStartHandler(e)}
-        onDragLeave={e=>dragLeaveHandler(e)}
-        onDragOver={e=>dragStartHandler(e)}
-        className="drop_area_default"
-      >Перетащите файлы, чтобы загрузить их</div>
-      }
-    </div>
-  )
+  onFileSelectedHandler = (e) => {
+    e.preventDefault()
+    e.preventDefault()
+    let files = [...e.target.files]
+    files.forEach(el => {
+      console.log(el)
+      const formData = new FormData()
+      formData.append('File',el,el.name);
+      this.sendImage(formData)
+    });
+  }
+  render() {
+    return (
+      <div className="container drop_area_parent" onClick={e=>this.inputRef.click()}>
+        {this.state.drag
+        ?<div 
+          onDragStart={e=>this.dragStartHandler(e)}
+          onDragLeave={e=>this.dragLeaveHandler(e)}
+          onDragOver={e=>this.dragStartHandler(e)}
+          onDrop={e=>this.onDropHandler(e)}
+          className="drop_area_drag"
+        >Отпустите файлы, чтобы загрузить их
+        </div>
+        :<div
+          onDragStart={e=>this.dragStartHandler(e)}
+          onDragLeave={e=>this.dragLeaveHandler(e)}
+          onDragOver={e=>this.dragStartHandler(e)}
+          className="drop_area_default"
+        >Перетащите файлы или кликните для выбора
+        </div>
+        }
+        <input onChange={e=>this.onFileSelectedHandler(e)} ref={(inputRef)=>{this.inputRef=inputRef}} multiple={true} type='file' hidden/>
+      </div>
+    )
+  }
 }
-export default DragAndDrop;
